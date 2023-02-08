b3b06f5a0cbd6cee744eb0976717ef1ee21cdaf3
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-08 17:17:25 +0500

    hi

--- codes/src/App.js
+++ codes/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/NavbarComponent';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar/>
     </div>
   );
 }
---
+++ codes/src/components/NavbarComponent/index.jsx
@@ -0,0 +1,18 @@
+import './style.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+        <img src="" alt="" />
+        <ul>
+            <li><h3>BOSH SAHIFA</h3></li>
+            <li><h3>BIZ HAQIMIZDA</h3></li>
+            <li><h3>BAJARILGAN ISHLAR</h3></li>
+            <li><h3>KONTAKTLAR</h3></li>
+            <li><button>UZ</button><button>RU</button></li>
+        </ul>
+    </div>
+  );
+}
+
+export default Navbar;
