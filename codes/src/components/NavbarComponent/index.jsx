0ac0e70c1615b5c37f1b1a76e969de93cc7ea450
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-08 18:54:12 +0500

    hi

--- codes/src/components/NavbarComponent/index.jsx
+++ codes/src/components/NavbarComponent/index.jsx
@@ -1,8 +1,20 @@
+import { useRef } from 'react';
 import './style.css';
 
 function Navbar() {
+    const BarsSide = useRef();
+    const btnOpen = useRef();
+    const btnClose = useRef();
+    btnClose.current.style.dislay = 'none'
     const HandleOpenSide = () =>{
-
+        btnClose.current.style.dislay = 'block'
+        btnOpen.current.style.dislay = 'none'
+        BarsSide.current.style.dislay = 'block'
+    }
+    const HandleCloseSide = () =>{
+        btnClose.current.style.dislay = 'none'
+        btnOpen.current.style.dislay = 'block'
+        BarsSide.current.style.dislay = 'none'
     }
   return (
     <>
@@ -17,10 +29,11 @@ function Navbar() {
         </ul>
     </div>
     <div className="BarsNav">
-        <button onClick={HandleOpenSide}><i className='fa-solid fa-bars'></i></button>
+        <button onClick={HandleOpenSide} ref='btnOpen'><i className='fa-solid fa-bars'></i></button>
+        <button onClick={HandleCloseSide} ref='btnClose'><i className='fa-solid fa-bars'></i></button>
         <img src="https://res.cloudinary.com/dnuh1ejtz/image/upload/v1675857963/download_fimwgc.png" alt="" />
     </div>
-    <div className="BarsSide">
+    <div className="BarsSide" ref='BarsSide'>
         <ul>
             <li><h3>BOSH SAHIFA</h3></li>
             <li><h3>BIZ HAQIMIZDA</h3></li>
