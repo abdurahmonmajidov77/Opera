cd6e6e07b0983543dd0808b03ac33c358a9a4348
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-08 18:19:43 +0500

    hi

--- codes/src/components/NavbarComponent/index.jsx
+++ codes/src/components/NavbarComponent/index.jsx
@@ -1,9 +1,13 @@
 import './style.css';
 
 function Navbar() {
+    const HandleOpenSide = () =>{
+
+    }
   return (
+    <>
     <div className="Navbar">
-        <img src="" alt="" />
+        <img src="https://res.cloudinary.com/dnuh1ejtz/image/upload/v1675857963/download_fimwgc.png" alt="" />
         <ul>
             <li><h3>BOSH SAHIFA</h3></li>
             <li><h3>BIZ HAQIMIZDA</h3></li>
@@ -12,6 +16,14 @@ function Navbar() {
             <li><button>UZ</button><button>RU</button></li>
         </ul>
     </div>
+    <div className="BarsNav">
+        <button onClick={HandleOpenSide}><i className='fa-solid fa-bars'></i></button>
+        <img src="https://res.cloudinary.com/dnuh1ejtz/image/upload/v1675857963/download_fimwgc.png" alt="" />
+    </div>
+    <div className="BarsSide">
+        
+    </div>
+    </>
   );
 }
 
