cae13f1cbae85e94f1fbe70244e78f7b0d1fded6
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-09 16:44:33 +0500

    changes

--- codes/src/components/HeaderComponent/index.jsx
+++ codes/src/components/HeaderComponent/index.jsx
@@ -3,10 +3,15 @@ import './style.css';
 function Header() {
   return (
     <div className="Header">
+        <div className="HeaderBox">
+        <div className="HeaderInbox">
         <img src="https://res.cloudinary.com/dnuh1ejtz/image/upload/v1675871898/ddvgdgvgv_dz7vaj.webp" alt="" />
         <p>Biz bizneslarni rivojlantiramiz,
 korxonalarga bozorda emotsional
 imij yaratishda yordam beramiz.</p>
+        </div>
+        <img className='HeaderImg' src="https://res.cloudinary.com/dnuh1ejtz/image/upload/v1675857922/cdvfdefvedf_lqcync.webp" alt="" />
+        </div>
     </div>
   );
 }
