9b31567ba64462772e2346c1d2b11dad1788cb01
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-18 22:36:30 +0500

    chnges css

--- codes/src/components/NavbarComponent/index.jsx
+++ codes/src/components/NavbarComponent/index.jsx
@@ -20,10 +20,10 @@ function Navbar() {
     <div className="Navbar">
         <img src="https://res.cloudinary.com/dnuh1ejtz/image/upload/v1675857963/download_fimwgc.png" alt="" />
         <ul>
-            <li><h3>BOSH SAHIFA</h3></li>
-            <li><h3>BIZ HAQIMIZDA</h3></li>
-            <li><h3>BAJARILGAN ISHLAR</h3></li>
-            <li><h3>KONTAKTLAR</h3></li>
+            <li><h3><button>BOSH SAHIFA</button></h3></li>
+            <li><h3><button>BIZ HAQIMIZDA</button></h3></li>
+            <li><h3><button>BAJARILGAN ISHLAR</button></h3></li>
+            <li><h3><button>KONTAKTLAR</button></h3></li>
             <li><button>UZ</button><button>RU</button></li>
         </ul>
     </div>
@@ -34,10 +34,10 @@ function Navbar() {
     </div>
     <div className="BarsSide" ref={barsSide}>
         <ul>
-            <li><h3>BOSH SAHIFA</h3></li>
-            <li><h3>BIZ HAQIMIZDA</h3></li>
-            <li><h3>BAJARILGAN ISHLAR</h3></li>
-            <li><h3>KONTAKTLAR</h3></li>
+            <li><h3><button>BOSH SAHIFA</button></h3></li>
+            <li><h3><button>BIZ HAQIMIZDA</button></h3></li>
+            <li><h3><button>BAJARILGAN ISHLAR</button></h3></li>
+            <li><h3><button>KONTAKTLAR</button></h3></li>
             <li><button>UZ</button><button>RU</button></li>
         </ul>
     </div>
