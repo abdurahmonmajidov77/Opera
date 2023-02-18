9bec1f7013050b601f1e510eec30f94418d26a2b
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-19 00:15:52 +0500

    hi

--- codes/src/App.js
+++ codes/src/App.js
@@ -9,7 +9,11 @@ function App() {
       <Header/>
       <button className='Tel'><i className='fa-solid fa-phone-volume'></i></button>
       <div className='modalTel'>
-        
+        <h3>Telefon raqamingizni qoldiring va biz sizga tez orada aloqaga chiqamiz</h3>
+        <input placeholder='+99893 000 00 00'/>
+        <buttn>Yuborish</buttn>
+        <p>Yoki siz bizga o'zingaz aloqaga chiqishingiz mumkin</p>
+        <h4>+99893 535 00 44</h4>
       </div>
     </div>
   );
