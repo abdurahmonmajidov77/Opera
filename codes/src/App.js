fdcb5ac1eca86f3fb1359a8aaee5bfaf598cc663
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-19 01:09:03 +0500

    up

--- codes/src/App.js
+++ codes/src/App.js
@@ -8,15 +8,27 @@ function App() {
   const ModalTel = useRef()
   const CloseModalUp = useRef()
   const OpenModal = () =>{
-
+    OpenModalUp.current.style.display = 'none'
+    CloseModalUp.current.style.display = 'block'
+    ModalTel.current.style.transform = 'scale(1)'
+  }
+  const CloseModal = () =>{
+    OpenModalUp.current.style.display = 'block'
+    CloseModalUp.current.style.display = 'none'
+    ModalTel.current.style.transform = 'scale(0)'
+  }
+  const HandleSubmt = () =>{
+    OpenModalUp.current.style.display = 'block'
+    CloseModalUp.current.style.display = 'none'
+    ModalTel.current.style.transform = 'scale(0)'
   }
   return (
     <div className="App">
       <Navbar/>
       <Header/>
       <button onClick={OpenModal} ref={OpenModalUp} className='Tel'><i className='fa-solid fa-phone-volume'></i></button>
-      <button onClick={OpenModal} ref={CloseModalUp} className='TelUp'><i className='fa-solid fa-xmark'></i></button>
-      <form className='modalTel' ref={ModalTel}>
+      <button onClick={CloseModal} ref={CloseModalUp} className='TelUp'><i className='fa-solid fa-xmark'></i></button>
+      <form className='modalTel' onSubmit={HandleSubmt} ref={ModalTel}>
         <h3>Telefon raqamingizni qoldiring va biz sizga tez orada aloqaga chiqamiz</h3>
         <input placeholder='+99893 000 00 00' required type="number"/>
         <button type='submit'>Yuborish</button>
